41f43bd5782c83440d1f58e74868c9293e3fdad9
Ilya Denisov <Fredperry.d1a@gmail.com>
2021-03-31 17:27:18 +0300

    work complete 142 page

---
+++ src/SwordJuggler.kt
@@ -0,0 +1,34 @@
+import java.lang.Exception
+import java.lang.IllegalStateException
+
+fun main(args: Array<String>) {
+    var swordsJuggling: Int? = null
+    val isJugglingProficient = (1..3).shuffled().last() == 3
+    if (isJugglingProficient) {
+        swordsJuggling = 2
+    }
+
+    try {
+        proficiencyCheck(swordsJuggling)
+        swordsJuggling = swordsJuggling!!.plus(1)
+    } catch (e: Exception) {
+        println(e)
+    }
+
+    proficiencyCheck(swordsJuggling)
+    swordsJuggling = swordsJuggling!!.plus(1)
+
+    println("You juggle $swordsJuggling swords!")
+}
+
+fun proficiencyCheck(swordsJuggling: Int?) {
+//    swordsJuggling ?: throw UnskilledSwordJugglerException()
+    checkNotNull(swordsJuggling, { "Player cannot juggle swords" })
+}
+
+    class UnskilledSwordJugglerException() :
+            IllegalStateException("Player cannot juggle swords")
+
+fun juggleSwords(swordsJuggling: Int) {
+    require(swordsJuggling >= 3, { "Juggle at least 3 swords to be exciting." })
+}
